aa49fc405217cdf8031471d5ff316b8bbbf01076
Fouzi92 <fouzi457@hotmail.fr>
2023-05-09 15:36:30 +0200

    Formation php-8

---
+++ index-3-math.php
@@ -0,0 +1,44 @@
+<?php
+ $nombre = 14;
+ $nombre2 = 87;
+
+ //Addition 
+ $resultat = $nombre + $nombre2;
+
+ // soustration 
+
+ $resultat = $nombre - $nombre2;
+
+ // Multiplication 
+
+ $resultat = $nombre * $nombre2;
+
+ // Division 
+
+ $resultat = $nombre / $nombre2;
+
+ // calcul "complexe"
+
+ $resultat = ($nombre + $nombre2) / 5;
+
+ // Modulo
+
+ $resultat = $nombre2 % $nombre;
+
+ // 9 est-il multiple de 3  ?
+ 
+ var_dump(9 % 3);
+ var_dump($resultat);
+  
+ // Incrémenter (ajouter 1)
+  
+  $nombre += 1;
+
+  var_dump(++$nombre);
+
+  // Décrémenter 
+  $nombre -= 1;
+  var_dump($nombre)
+
+
+ ?>
